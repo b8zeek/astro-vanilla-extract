4d4300ddae6ec7e537434e98f7ca53890893a620
Mirko Basic <bejzik8@gmail.com>
2023-11-07 20:42:48 +0100

    Updated Background component.

--- src/components/Background/Background.css.ts
+++ src/components/Background/Background.css.ts
@@ -3,15 +3,25 @@ import { style } from '@vanilla-extract/css'
 import { vars } from '../../styles/vars.css'
 
 export const background = style({
+    width: '100%',
+    height: '100vh',
     position: 'absolute',
     top: 0,
     left: 0,
-    bottom: 0,
-    right: 0,
     zIndex: -1,
     backgroundImage: `radial-gradient(
-      30% 10% at top right,
+      30% 30% at center right,
       ${vars.no3elementBackground} 0%,
       ${vars.no1appBackground} 100%
-  )`
+    )`,
+
+    '@media': {
+        'screen and (min-width: 1024px)': {
+            backgroundImage: `radial-gradient(
+              30% 10% at top right,
+              ${vars.no3elementBackground} 0%,
+              ${vars.no1appBackground} 100%
+            )`
+        }
+    }
 })
