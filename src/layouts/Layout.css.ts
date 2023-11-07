e8120da9f53de3d98541351db47125d10ee08a6f
Mirko Basic <bejzik8@gmail.com>
2023-11-07 14:25:57 +0100

    Updated Layout.css.

--- src/layouts/Layout.css.ts
+++ src/layouts/Layout.css.ts
@@ -1,8 +1,26 @@
 import { style } from '@vanilla-extract/css'
 
+import { vars } from '../styles/vars.css'
+
 export const main = style({
     maxWidth: 720,
     width: '100%',
     minHeight: '100vh',
+    padding: '2rem',
+    borderRadius: 8,
     margin: '0 auto'
+})
+
+export const background = style({
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    zIndex: -1,
+    backgroundImage: `radial-gradient(
+        50% 80% at top left,
+        ${vars.no3elementBackground} 0%,
+        ${vars.no1appBackground} 100%
+    )`
 })
