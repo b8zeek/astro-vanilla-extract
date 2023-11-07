ac297e1fd26e802fc4c0f3882bf02d61f7cc6362
Mirko Basic <bejzik8@gmail.com>
2023-11-07 14:25:42 +0100

    Updated global styles.

--- src/layouts/reset.css.ts
+++ src/layouts/reset.css.ts
@@ -11,7 +11,7 @@ globalStyle('*', {
 globalStyle('body', {
     minWidth: '320px',
     minHeight: '100vh',
-    backgroundColor: vars.no1appBackground
+    background: vars.no1appBackground
 })
 
 globalStyle(':root', {
@@ -24,4 +24,27 @@ globalStyle(':root', {
     MozOsxFontSmoothing: 'grayscale',
     WebkitTextSizeAdjust: '100%',
     margin: '0 auto'
+})
+
+globalStyle('h1', {
+    lineHeight: '3.25rem',
+    fontSize: '3rem',
+    color: vars.no12textContrastHigh,
+    marginBottom: '1rem'
+})
+
+globalStyle('h2', {
+    lineHeight: '1.5rem',
+    fontSize: '1.75rem',
+    color: vars.no12textContrastHigh,
+    marginBottom: '1.25rem'
+})
+
+globalStyle('p', {
+    lineHeight: 1.8,
+    fontSize: '1.15rem',
+    letterSpacing: '-0.01em',
+    textAlign: 'justify',
+    color: vars.no11textContrastLow,
+    marginBottom: '1rem'
 })
