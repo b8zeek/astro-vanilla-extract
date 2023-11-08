ccffc4b1b636fdc0771c9964143da7c257387ef8
Mirko Basic <bejzik8@gmail.com>
2023-11-08 23:44:55 +0100

    Created PersonCard component.

---
+++ src/components/PersonCard/PersonCard.css.ts
@@ -0,0 +1,30 @@
+import { style } from '@vanilla-extract/css'
+
+import { vars } from '../../styles/vars.css'
+
+export const card = style({
+    display: 'flex',
+    alignItems: 'center',
+    gap: '0.5rem',
+    paddingBottom: '1rem',
+    borderBottom: `1px solid ${vars.no6bordersNonInteractive}`,
+    marginBottom: '1rem'
+})
+
+export const cardHeading = style({
+    lineHeight: 1,
+    margin: 0
+})
+
+export const cardSubheading = style({
+    lineHeight: 1,
+    fontSize: '1rem',
+    color: vars.no11textContrastLow,
+    margin: 0
+})
+
+export const avatar = style({
+    width: '4rem',
+    borderRadius: '50%',
+    border: `4px solid ${vars.no6bordersNonInteractive}`
+})
