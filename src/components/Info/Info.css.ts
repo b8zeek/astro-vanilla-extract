54b6ed7c23b3e82623308423213a5290435212c0
Mirko Basic <bejzik8@gmail.com>
2023-11-08 22:06:14 +0100

    Updated Info component styling.

--- src/components/Info/Info.css.ts
+++ src/components/Info/Info.css.ts
@@ -3,6 +3,7 @@ import { style } from '@vanilla-extract/css'
 import { vars } from '../../styles/vars.css'
 
 export const info = style({
+    position: 'relative',
     zIndex: 1,
     display: 'flex',
     alignItems: 'center',
@@ -12,8 +13,8 @@ export const info = style({
         ${vars.no1appBackground} 100%
     )`,
     borderBottom: `1px solid ${vars.no6bordersNonInteractive}`,
-    padding: '0 1.5rem',
-    position: 'relative',
+    padding: '7rem 1.5rem',
+    overflow: 'hidden',
 
     '@media': {
         'screen and (min-width: 1024px)': {
@@ -39,13 +40,14 @@ export const infoContent = style({
     width: '32rem',
     margin: '0 auto',
     borderRadius: '1rem',
-    padding: '2rem 3rem',
     backdropFilter: 'blur(3px)',
+    padding: '2rem',
     boxShadow: '3px 3px 1rem rgba(0, 0, 0, 0.2)',
 
     '@media': {
         'screen and (min-width: 1024px)': {
             width: '24rem',
+            padding: '2rem 3rem',
             margin: 0
         }
     }
