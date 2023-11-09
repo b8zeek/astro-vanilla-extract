15d307042511230f29fa39d4d36ca897361c2790
Mirko Basic <bejzik8@gmail.com>
2023-11-09 09:43:08 +0100

    Updated Galaxy component.

--- src/components/Info/Something.css.ts
+++
@@ -1,7 +0,0 @@
-import { style } from '@vanilla-extract/css'
-
-export const transform = style({
-    transform:
-        'translate(0, 0) rotate(30deg) skewX(0) skewY(0) scaleX(1) skalY(1)',
-    transformOrigin: 'top right'
-})
