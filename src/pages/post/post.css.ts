ae73e92bf4047a4b44b39c1b4cc18ce73742fd16
Mirko Basic <bejzik8@gmail.com>
2023-11-09 18:37:13 +0100

    Updated single post component.

---
+++ src/pages/post/post.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css'
+import { vars } from '../../styles/vars.css'
+
+export const anchor = style({
+    display: 'inline-block',
+    padding: '0.5rem 2rem',
+    color: vars.no11textContrastLow,
+    backgroundImage: `linear-gradient(${vars.no3elementBackground}, ${vars.no4elementBackgroundHovered})`,
+    borderRadius: '0.5rem',
+    border: `1px solid ${vars.no7bordersInteractive}`,
+    marginBottom: '4rem'
+})
