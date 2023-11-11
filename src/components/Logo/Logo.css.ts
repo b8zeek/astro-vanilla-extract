06fe6df38265ac1e7d59b9c79df24a5ffaf32726
Mirko Basic <bejzik8@gmail.com>
2023-11-11 15:10:38 +0100

    Updated Logo component.

--- src/components/Logo/Logo.css.ts
+++ src/components/Logo/Logo.css.ts
@@ -2,9 +2,9 @@ import { style } from '@vanilla-extract/css'
 
 export const logo = style({
     display: 'block',
+    fontSize: '2.5rem',
     letterSpacing: '-0.2rem',
     whiteSpace: 'nowrap',
-    fontSize: '2.5rem',
     backgroundImage:
         'linear-gradient(71.18deg, rgb(0, 34, 255) -27.32%, rgb(0, 34, 255) -16.39%, rgb(81, 121, 254) -7.38%, rgb(165, 237, 182) 30.59%, rgb(250, 232, 90) 46.06%, rgb(253, 172, 62) 62.61%, rgb(255, 92, 0) 75.82%)',
     backgroundClip: 'text',
@@ -24,9 +24,13 @@ export const infoComponentLogoStyles = style({
     }
 })
 
-export const layoutComponentLogoStyles = style({
-    position: 'absolute',
-    zIndex: 3,
-    top: '1rem',
-    left: '1.5rem'
+export const headerComponentLogoStyles = style({
+    display: 'none',
+
+    '@media': {
+        'screen and (min-width: 1024px)': {
+            display: 'block',
+            fontSize: '2rem'
+        }
+    }
 })
