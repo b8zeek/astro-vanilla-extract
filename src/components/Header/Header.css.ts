a141553c0c58be0ac6203e594cde18b58cb59e4a
Mirko Basic <bejzik8@gmail.com>
2023-11-12 11:44:26 +0100

    Updated Header styles.

---
+++ src/components/Header/Header.css.ts
@@ -0,0 +1,8 @@
+import { style } from '@vanilla-extract/css'
+
+import { vars } from '../../styles/vars.css'
+
+export const anchor = style({
+    textDecoration: 'none',
+    color: vars.no11textContrastLow
+})
