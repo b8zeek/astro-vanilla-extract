1f25287c536a9c8cb66abfae48dae1cd9036a745
Mirko Basic <bejzik8@gmail.com>
2023-11-13 12:20:37 +0100

    Updated Header component.

--- src/components/Header/Header.css.ts
+++ src/components/Header/Header.css.ts
@@ -3,6 +3,8 @@ import { style } from '@vanilla-extract/css'
 import { vars } from '../../styles/vars.css'
 
 export const anchor = style({
+    display: 'inline-flex',
+    alignItems: 'center',
     textDecoration: 'none',
     color: vars.no11textContrastLow
 })
