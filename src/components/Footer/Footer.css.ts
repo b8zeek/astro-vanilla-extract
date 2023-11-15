a30c1e135e50b29b165ac8967e9127c99d03facd
Mirko Basic <bejzik8@gmail.com>
2023-11-15 14:28:49 +0100

    Updated Footer styling.

--- src/components/Footer/Footer.css.ts
+++ src/components/Footer/Footer.css.ts
@@ -1,7 +1,13 @@
 import { style } from '@vanilla-extract/css'
+import { vars } from '../../styles/vars.css'
 
-export const footer = style({})
+export const footer = style({
+    padding: '5rem 0',
+    backgroundImage: `linear-gradient(${vars.no2subtleBackground}, ${vars.no1appBackground})`
+})
 
 export const paragraph = style({
+    fontSize: '0.75rem',
+    textAlign: 'center',
     margin: 0
 })
