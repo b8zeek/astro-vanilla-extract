933a1ea8584289f7eb43a28856406d2c81bc6018
Mirko Basic <bejzik8@gmail.com>
2023-11-24 01:37:26 +0300

    Added absolute imports support. 🔧

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,25 @@
 import { defineConfig } from 'astro/config'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import * as path from 'path'
+
+const folders = [
+    '',
+    'assets',
+    'components',
+    'models',
+    'pages',
+    'services',
+    'styles'
+]
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [vanillaExtractPlugin()]
+        plugins: [vanillaExtractPlugin()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src')
+            }
+        }
     }
 })
