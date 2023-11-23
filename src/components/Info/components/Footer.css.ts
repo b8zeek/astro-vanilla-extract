890ff854b4ecd59967ac130848f63abbf0eb0e29
Mirko Basic <bejzik8@gmail.com>
2023-11-23 23:03:41 +0300

    Created Footer styles. 📝

---
+++ src/components/Info/components/Footer.css.ts
@@ -0,0 +1,25 @@
+import { style } from '@vanilla-extract/css'
+import { vars } from '../../../styles/vars.css'
+
+export const footer = style({
+    display: 'none',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    padding: '0 0 0.5rem 3rem',
+
+    '@media': {
+        'screen and (min-width: 1024px)': {
+            display: 'block'
+        }
+    }
+})
+
+export const paragraph = style({
+    lineHeight: 2,
+    fontSize: '0.6rem',
+    color: vars.no9solidBackground,
+    textAlign: 'center',
+    margin: 0
+})
