4faee78bcd9c7069910f4f195c9e86d6f3507768
Mirko Basic <bejzik8@gmail.com>
2023-11-24 15:15:11 +0300

    Updated project configuration. ⚙️

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,9 @@
 import { defineConfig } from 'astro/config'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
-import * as path from 'path'
-
-const folders = [
-    '',
-    'assets',
-    'components',
-    'models',
-    'pages',
-    'services',
-    'styles'
-]
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [vanillaExtractPlugin()],
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src')
-            }
-        }
+        plugins: [vanillaExtractPlugin()]
     }
 })
